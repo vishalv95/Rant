a8f60b0fc722bd3b362192c0d5f7db1d6985b096
Vishal Vusirikala <vishv95@bes.cs.utexas.edu>
2016-10-18 18:14:57 -0500

    Initial Commit

---
+++ Rant/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Rant
+//
+//  Created by Vishal Vusirikala on 10/18/16.
+//  Copyright © 2016 group4. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
