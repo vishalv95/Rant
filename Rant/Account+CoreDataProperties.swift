91196c164cf2921d8a66707f99f6ec65620f15be
vvusirikala <vvusirikala@rmn.com>
2016-11-01 22:34:46 -0500

    Fix core data bug

--- Rant/Account+CoreDataProperties.swift
+++ Rant/Account+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Account+CoreDataProperties.swift
 //  Rant
 //
-//  Created by Vishal Vusirikala on 10/29/16.
+//  Created by Vishal Vusirikala on 11/1/16.
 //  Copyright © 2016 group4. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,11 +16,11 @@ extension Account {
 
     @NSManaged var city: String?
     @NSManaged var password: String?
+    @NSManaged var profileImage: NSData?
     @NSManaged var state: String?
     @NSManaged var user: String?
     @NSManaged var comment: NSSet?
     @NSManaged var favorites: NSSet?
     @NSManaged var rant: NSSet?
-    @NSManaged var profileImage: NSData?
 
 }
--- Rant/Rant+CoreDataProperties.swift
+++ Rant/Rant+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Rant+CoreDataProperties.swift
 //  Rant
 //
-//  Created by Vishal Vusirikala on 10/27/16.
+//  Created by Vishal Vusirikala on 11/1/16.
 //  Copyright © 2016 group4. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -22,4 +22,5 @@ extension Rant {
     @NSManaged var upvotes: NSNumber?
     @NSManaged var account: Account?
     @NSManaged var comment: NSSet?
+
 }
